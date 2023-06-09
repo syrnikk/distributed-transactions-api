e00e9f8d62dd8479dfc6267f0fa711f6a767a2d9
Kacper Syrnik <kacper.syrnik1@gmail.com>
2023-06-09 14:28:53 +0200

    Initial commit

---
+++ src/main/java/com/syrnik/DistributedTransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.syrnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTransactionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syrnik/DistributedTransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTransactionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
